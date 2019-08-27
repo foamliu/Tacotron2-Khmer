6c830861b28faf74218256c674b80675f62e2a40
Yang Liu <liuyang12@focusmedia.cn>
2019-08-27 17:18:34 +0800

    update

---
+++ test/test_total_length.py
@@ -0,0 +1,19 @@
+import os
+
+import librosa
+
+folder = '../data/km_kh_male/wavs'
+files = [f for f in os.listdir(folder) if f.endswith('.wav')]
+
+sampling_rate = 22050
+
+total_duration = 0
+
+for file in files:
+    fullpath = os.path.join(folder, file)
+    y, sr = librosa.core.load(fullpath, sampling_rate)
+    yt, index = librosa.effects.trim(y, top_db=60)
+    d = librosa.get_duration(yt)
+    total_duration += d
+
+print('{:.4f} hours'.format(total_duration / 3600))
